3abab812fa4db97d1743fb658c9b8895fae8b3e2
Ощепков Иван <ivanvlado@MacBook-Pro-Osepkov.local>
2018-05-31 16:45:18 +0300

    test sidebar renders using 'active' prop

--- tests/Sidebar.test.js
+++ tests/Sidebar.test.js
@@ -2,8 +2,20 @@ import React from 'react';
 import Sidebar from '../src/';
 
 describe('Sidebar', () => {
-    it('Should renders without problems', () => {
+    it('Should not renders by default', () => {
+        const sidebar = shallow(<Sidebar />);
+        
+        expect(sidebar).toMatchSnapshot();
+    });
+
+    it('Should renders if active', () => {
         const sidebar = shallow(<Sidebar active={true}/>);
+
+        expect(sidebar).toMatchSnapshot();
+    });
+
+    it('Should not renders if not active', () => {
+        const sidebar = shallow(<Sidebar active={false}/>);
 
         expect(sidebar).toMatchSnapshot();
     });
