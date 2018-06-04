5fd5ee2da850ab10fb37a6803d29ee9530477967
Ощепков Иван <ivanvlado@MacBook-Pro.local>
2018-06-04 17:07:34 +0300

    Rename imported name to package name in demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-import Example from '../../src';
+import Sidebar from '../../src';
 
 class Demo extends Component {
     constructor(props) {
@@ -26,7 +26,7 @@ class Demo extends Component {
             <div>
                 <h1>react-sidebar Demo</h1>
                 <button onClick={this.handleShowSidebar}>Show sidebar</button>
-                <Example active={this.state.sidebarActive} onClose={this.handleCloseSidebar}>
+                <Sidebar active={this.state.sidebarActive} onClose={this.handleCloseSidebar}>
                     Sidebar content
                     <p>Tempor sint adipisicing aliquip fugiat quis eu qui cillum nisi cillum. Ad ad amet aliquip mollit ut eu occaecat deserunt ullamco officia duis amet. Cupidatat reprehenderit voluptate mollit sit sunt sint id proident sunt ullamco cillum nostrud id proident. Est elit do commodo dolore sit. Et Lorem tempor ex eu sunt mollit magna aliqua consequat pariatur ullamco Lorem ullamco. Incididunt in excepteur est in aute qui commodo exercitation esse.</p>
                     <p>Tempor sint adipisicing aliquip fugiat quis eu qui cillum nisi cillum. Ad ad amet aliquip mollit ut eu occaecat deserunt ullamco officia duis amet. Cupidatat reprehenderit voluptate mollit sit sunt sint id proident sunt ullamco cillum nostrud id proident. Est elit do commodo dolore sit. Et Lorem tempor ex eu sunt mollit magna aliqua consequat pariatur ullamco Lorem ullamco. Incididunt in excepteur est in aute qui commodo exercitation esse.</p>
@@ -35,7 +35,7 @@ class Demo extends Component {
                     <p>Tempor sint adipisicing aliquip fugiat quis eu qui cillum nisi cillum. Ad ad amet aliquip mollit ut eu occaecat deserunt ullamco officia duis amet. Cupidatat reprehenderit voluptate mollit sit sunt sint id proident sunt ullamco cillum nostrud id proident. Est elit do commodo dolore sit. Et Lorem tempor ex eu sunt mollit magna aliqua consequat pariatur ullamco Lorem ullamco. Incididunt in excepteur est in aute qui commodo exercitation esse.</p>
                     <p>Tempor sint adipisicing aliquip fugiat quis eu qui cillum nisi cillum. Ad ad amet aliquip mollit ut eu occaecat deserunt ullamco officia duis amet. Cupidatat reprehenderit voluptate mollit sit sunt sint id proident sunt ullamco cillum nostrud id proident. Est elit do commodo dolore sit. Et Lorem tempor ex eu sunt mollit magna aliqua consequat pariatur ullamco Lorem ullamco. Incididunt in excepteur est in aute qui commodo exercitation esse.</p>
                     <p>Tempor sint adipisicing aliquip fugiat quis eu qui cillum nisi cillum. Ad ad amet aliquip mollit ut eu occaecat deserunt ullamco officia duis amet. Cupidatat reprehenderit voluptate mollit sit sunt sint id proident sunt ullamco cillum nostrud id proident. Est elit do commodo dolore sit. Et Lorem tempor ex eu sunt mollit magna aliqua consequat pariatur ullamco Lorem ullamco. Incididunt in excepteur est in aute qui commodo exercitation esse.</p>
-                </Example>
+                </Sidebar>
                 <p>Laboris et voluptate cupidatat exercitation Lorem in aliquip duis aute ut ex nisi. Commodo nostrud aute velit adipisicing. Ipsum incididunt ea aliqua sunt id labore dolor laborum consectetur. Minim cillum culpa dolore ullamco nisi ex deserunt excepteur excepteur eu. Fugiat nisi dolor aliquip nulla et aute ad et commodo ut elit. Aliquip veniam nostrud est commodo officia magna sint magna proident. Duis esse aliqua esse excepteur consequat.</p>
                 <p>Laboris et voluptate cupidatat exercitation Lorem in aliquip duis aute ut ex nisi. Commodo nostrud aute velit adipisicing. Ipsum incididunt ea aliqua sunt id labore dolor laborum consectetur. Minim cillum culpa dolore ullamco nisi ex deserunt excepteur excepteur eu. Fugiat nisi dolor aliquip nulla et aute ad et commodo ut elit. Aliquip veniam nostrud est commodo officia magna sint magna proident. Duis esse aliqua esse excepteur consequat.</p>
                 <p>Laboris et voluptate cupidatat exercitation Lorem in aliquip duis aute ut ex nisi. Commodo nostrud aute velit adipisicing. Ipsum incididunt ea aliqua sunt id labore dolor laborum consectetur. Minim cillum culpa dolore ullamco nisi ex deserunt excepteur excepteur eu. Fugiat nisi dolor aliquip nulla et aute ad et commodo ut elit. Aliquip veniam nostrud est commodo officia magna sint magna proident. Duis esse aliqua esse excepteur consequat.</p>
