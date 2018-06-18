3820db1d8e293be35fa14ce0d010f02e106e9f55
Alexander Berezovsky <fantik217@gmail.com>
2018-06-18 13:52:29 +0700

    theme

--- src/styled/Blocker.js
+++ src/styled/Blocker.js
@@ -1,29 +1,33 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { getThemeByKeys } from '../utils';
-import defaultTheme from '../theme/defaultTheme';
+import { getThemeByKeys, innerMerge } from "../utils";
+import defaultTheme from "../theme/defaultTheme";
 
 const Elem = styled.div`
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background-color: ${props => props.backgroundColor};
-    opacity: 0;
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: ${props => props.backgroundColor};
+  opacity: 0;
 
-    .active & {
-        opacity: 1;
-    }
+  .active & {
+    opacity: 1;
+  }
 `;
 
 const Blocker = props => {
-    const theme = getThemeByKeys(
-        (props.theme && props.theme.blocker) || defaultTheme.blocker
-    );
+  const merged = innerMerge(
+    {},
+    defaultTheme.Sidebar.blocker,
+    (props.theme && props.theme.Sidebar && props.theme.Sidebar.blocker) || {}
+  );
 
-    return <Elem {...theme} {...props} />
-}
+  const theme = getThemeByKeys(merged);
+
+  return <Elem {...theme} {...props} />;
+};
 
 export default Blocker;
--- src/styled/Wrapper.js
+++ src/styled/Wrapper.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { getThemeByKeys } from '../utils';
-import defaultTheme from '../theme/defaultTheme';
+import { getThemeByKeys, innerMerge } from "../utils";
+import defaultTheme from "../theme/defaultTheme";
 
 const Elem = styled.div`
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    z-index: ${props => props.zIndex};
-    visibility: hidden;
-    transition: visibility 0.1s;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: ${props => props.zIndex};
+  visibility: hidden;
+  transition: visibility 0.1s;
 
-    &.active {
-        visibility: visible;
-    }
+  &.active {
+    visibility: visible;
+  }
 `;
 
 const Wrapper = props => {
-    const theme = getThemeByKeys(
-        (props.theme && props.theme.wrapper) || defaultTheme.wrapper
-    );
+  const merged = innerMerge(
+    {},
+    defaultTheme.Sidebar.wrapper,
+    (props.theme && props.theme.Sidebar && props.theme.Sidebar.wrapper) || {}
+  );
 
-    return <Elem {...theme} {...props} />
+  const theme = getThemeByKeys(merged);
+
+  return <Elem {...theme} {...props} />;
 };
 
 export default Wrapper;
